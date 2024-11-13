ae93b17ff944390838de3bc4a87423cbfde67fe0
Qk29 <qkbn2001@gmail.com>
2024-11-13 12:08:31 +0700

    backend quanly san pham

---
+++ admin/models/Product.php
@@ -0,0 +1,12 @@
+<?php 
+    class Product {
+        public $product_id;
+        public $product_name;
+        public $product_description;
+        public $product_price;
+        public $product_image;
+        public $categorie_id;
+
+    }
+
+?>
